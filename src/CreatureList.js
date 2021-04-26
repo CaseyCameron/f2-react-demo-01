abe68f37ac770439b0d77e3d892488832f9408b6
Casey Cameron <mcasey.cameron@gmail.com>
2021-04-26 14:13:16 -0700

    adding stuff

---
+++ src/CreatureList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import CreatureItem from './CreatureItem';
+import '.CreatureList.css';
+
+class CreatureList extends Component {
+  render() {
+    const creatures = this.props.creatures;
+
+    return (
+      <ul className="CreatureList">
+        {creatures.map(creature => (
+          <CreatureItem key={creature.tile} creature={creature}/>        
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default CreatureList;
